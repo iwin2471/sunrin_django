eab4c16eaadfeeeebccd0ecfe0a0f61202cb057d
iwin2471 <iwin12372@gmail.com>
2017-11-09 11:54:22 +0900

    add router

--- 20509_django/firstapp/views.py
+++ 20509_django/firstapp/views.py
@@ -4,4 +4,7 @@ def post_list(req):
     posts = Post.objects.all()
     return render(req, 'firstapp\home.html', {'posts':posts})
 
+def second(req):
+    return render(req, 'firstapp\second.html')
+
 # Create your views here.

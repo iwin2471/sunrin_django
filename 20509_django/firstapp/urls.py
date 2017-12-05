d9601853765d2dfd78c635ffa3da2db1de4c6c39
iwin2471 <iwin12372@gmail.com>
2017-12-05 11:28:06 +0900

    add html

--- 20509_django/firstapp/urls.py
+++ 20509_django/firstapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'second/', views.second, name='second'),
+    url(r'introduce/', views.intro, name='introduce'),
+    url(r'study/', views.study, name='study'),
+    url(r'google/', views.study, name='google'),
     ]
--- 20509_django/firstapp/views.py
+++ 20509_django/firstapp/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from .models import Post
 def post_list(req):
     posts = Post.objects.all()
-    return render(req, 'firstapp\home.html', {'posts':posts})
+    return render(req, 'firstapp\main.html', {'posts':posts})
 
 def second(req):
     return render(req, 'firstapp\second.html')
+
+def intro(req):
+    return render(req, 'firstapp\introduce.html')
+
+def study(req):
+    return render(req, 'firstapp\study.html')
 
 # Create your views here.

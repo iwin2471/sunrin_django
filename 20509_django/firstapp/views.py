75213237ab844bd84ca670573e0e91dcc228f23c
iwin2471 <iwin12372@gmail.com>
2017-12-07 10:55:28 +0900

    [form] update

--- 20509_django/firstapp/views.py
+++ 20509_django/firstapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
+
 def post_list(req):
     posts = Post.objects.all()
     return render(req, 'firstapp\main.html', {'posts':posts})
@@ -15,5 +17,24 @@ def study(req):
 
 def google(req):
     return render(req, 'firstapp\google.html')
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'firstapp\post_edit.html', {'form': form})
+
+def post_detail(req):
+    return render(req, 'firstapp\detail.html')
+def post(req):
+    if req.method == "POST ":
+        form = PostForm(req.POST)
+        if form.is_vaild():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('firstapp.views.post_detail', post_id=poost.pk)
+    else:
+        form = PostForm()
+    return render(req, 'firstapp\form.html', {'form': form})
 
 # Create your views here.
